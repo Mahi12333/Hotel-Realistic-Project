8f521c8e23c40e1e35a6da65a8eb2c2f1ba42b49
Mahitosh Giri <88604119+Mahi12333@users.noreply.github.com>
2024-06-15 17:19:17 +0530

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
-
+console.log(window.location.origin)
 const baseQuery  = fetchBaseQuery({baseUrl:`${window.location.origin}/`})
-
+console.log(baseQuery)
 export const apiSlice = createApi({
     baseQuery,
     tagTypes:['User'],
