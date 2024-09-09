918e8e7fc5417c996845cb519a49ae18fba68765
Mahitosh Giri <mahitoshgiri287@gmail.com>
2024-09-09 23:42:52 +0530

    Feature: All Feature completec

--- index.js
+++ index.js
@@ -33,7 +33,23 @@ app.use(cookieParser());
 //     }
 // )))
 
-app.use(cors({ origin: 'https://hotel-realistic-project.vercel.app', credentials: true }))
+
+const allowedOrigins = process.env.NODE_ENV === 'production'
+    ? ['https://hotel-realistic-project.vercel.app','http://localhost:3000','http://127.0.0.1:5500','http://127.0.0.1:5501','http://localhost:5000', 'http://localhost:3001', 'http://localhost:3002']
+    : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002','http://127.0.0.1:5500','http://127.0.0.1:5501'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 app.use('/api/users', userRoutes);
 app.use('/api/users', ramRoutes);
